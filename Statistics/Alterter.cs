45af99e4793fb7fcb3d9b0cfc9551848ab3cfa1a
Akhilahebbar <40620412+Akhilahebbar@users.noreply.github.com>
2021-01-27 16:58:44 +0530

    added alerter file

---
+++ Statistics/Alterter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Statistics
+{
+    public interface IAlerter
+    {
+
+
+    }
+
+    public class StatsAlerter
+    {
+        public float MaxThreshold;
+        readonly EmailAlert _emailAlert;
+        readonly LEDAlert ledAlert;
+        public StatsAlerter(float maxThreshold, IAlerter[] alerter)
+        {
+            this.MaxThreshold = maxThreshold;
+            this._emailAlert = (Statistics.EmailAlert)alerter[0];
+            this.ledAlert = (Statistics.LEDAlert)alerter[1];
+        }
+       public void checkAndAlert(List<double> listValues)
+        {
+            foreach (var value in listValues.Where(value => value > MaxThreshold))
+            {
+                _emailAlert.emailSent = true;
+                ledAlert.ledGlows = true;
+            }
+        }
+
+    }
+   public class EmailAlert:IAlerter
+    {
+        public Boolean emailSent = false;
+    }
+   public class LEDAlert: IAlerter
+    {
+        public Boolean ledGlows = false;
+    }
+}
