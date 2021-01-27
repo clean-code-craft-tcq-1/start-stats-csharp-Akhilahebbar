446e5d987140beab174c8868c7099c37fa581ecd
Akhilahebbar <40620412+Akhilahebbar@users.noreply.github.com>
2021-01-27 17:01:21 +0530

    implemented test cases

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,12 +1,31 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Statistics
 {
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+        public double average;
+        public double max;
+        public double min;
+
+        public StatsComputer CalculateStatistics(List<double> numbers)
+        {
+            if (numbers.Any(predicate: doubleValue => Double.IsNaN(doubleValue)))
+            {
+                return null;
+            }
+            else
+            {
+                //Implement statistics 
+                this.min = numbers.Min();
+                this.max = numbers.Max();
+                this.average = numbers.Average();
+            }
+            return this;
         }
     }
+
 }
+
